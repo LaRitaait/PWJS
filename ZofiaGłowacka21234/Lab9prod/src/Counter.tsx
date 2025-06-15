892aaf3dc187c189a55a23054f75c94f7ebaf3c8
LaRitaait <glowac.zofia@gmail.com>
2025-06-15 22:22:27 +0200

    Create Counter.tsx

---
+++ ZofiaGłowacka21234/Lab9prod/src/Counter.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default function Counter({ onLog }: { onLog: () => void }) {
+  console.log("Counter zrenderowany")
+  return <button onClick={onLog}>Pokaż licznik</button>
+}
