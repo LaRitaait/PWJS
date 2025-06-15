367a2a5f06611cd57046661bf107db3229279ce3
LaRitaait <glowac.zofia@gmail.com>
2025-06-15 22:22:47 +0200

    Create Exercise1.js

---
+++ ZofiaGłowacka21234/Lab9prod/src/Exercise1.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Exercise1() {
+  return (
+    <div>
+      <button onClick={() => console.log("Kliknięto przycisk!")}>
+        Kliknij mnie
+      </button>
+    </div>
+  )
+}
